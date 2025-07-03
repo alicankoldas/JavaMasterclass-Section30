55850988b5b75cf49ee1987b00ec89d01b4427b5
alicankoldas <koldasali@gmail.com>
2025-07-03 19:58:16 +0200

    FirstStepsJDK11

---
+++ Tutorials/FloatAndDouble/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " + myMaxDoubleValue);
+
+        int myIntValue = 5 / 2;
+        float myFloatValue = 5f / 2f;
+        double myDoubleValue = 5d / 2d;
+        System.out.println("MyIntValue= " + myIntValue);
+        System.out.println("MyFloatValue= " + myFloatValue);
+        System.out.println("MyDoubleValue= " + myDoubleValue);
+
+        int pounds = 20;
+        double kilogram = pounds * 0.45359237;
+        System.out.println(kilogram);
+    }
+}
